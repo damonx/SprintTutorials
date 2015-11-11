d26fa41c457c958cf4f06f3b2f61d86cef7c0b62
Damon Xu <damon.xu@calix.com>
2015-11-11 21:54:59 +1300

    b

---
+++ LooselyCoupledExample/src/main/java/com/mkyong/core/App.java
@@ -0,0 +1,17 @@
+package com.mkyong.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mkyong.output.OutputHelper;
+
+public class App {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "Spring-Common.xml" });
+
+		OutputHelper output = (OutputHelper) context.getBean("OutputHelper");
+		output.generateOutput();
+
+	}
+}
