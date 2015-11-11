83cc7758ef0d03fe935d3df05397632ad44f1330
Damon Xu <damon.xu@calix.com>
2015-11-11 21:27:18 +1300

    a

--- HelloWorldExample/src/main/java/com/mkyong/core/App.java
+++ HelloWorldExample/src/main/java/com/mkyong/core/App.java
@@ -4,8 +4,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+	private static ApplicationContext context;
+
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
+		context = new ClassPathXmlApplicationContext(
 				"SpringBeans.xml");
 
 		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
