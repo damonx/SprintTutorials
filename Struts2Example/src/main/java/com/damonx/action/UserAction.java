1d9bb68a81458f151cbff954ce4bf38aa0ee5d5b
Damon Xu <damon.xu@orionhealth.com>
2015-12-10 09:16:31 +1300

    new

---
+++ Struts2Example/src/main/java/com/damonx/action/UserAction.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Orchestral Developments Ltd and the Orion Health group of companies (2001 - 2015).
+ *
+ * This document is copyright. Except for the purpose of fair reviewing, no part
+ * of this publication may be reproduced or transmitted in any form or by any
+ * means, electronic or mechanical, including photocopying, recording, or any
+ * information storage and retrieval system, without permission in writing from
+ * the publisher. Infringers of copyright render themselves liable for
+ * prosecution.
+ */
+package com.damonx.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	private String username;
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String execute() {
+		return SUCCESS;
+	}
+}
